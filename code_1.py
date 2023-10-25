21b416c9a0cf346cb66a3b081b7cd736893733d1
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-10-26 01:46:54 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,29 @@
+        for (int item : data) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
 
-    return data
-if __name__ == "__main__":
-    for item in data:
-import random
-def generate_random_data():
+        for (int i = 0; i < 10; i++) {
+        }
+        }
 
-def main():
+    }    for (let i = 0; i < 10; i++) {
 
-    main()
+        data.push(Math.floor(Math.random() * 100) + 1);
 
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
+    data.forEach(item => {
+function generateRandomData() {
+        console.log("Random Number: " + item);
+
+}
+main();
+    const data = generateRandomData();
+    });
+    const data = [];
+    }
+    return data;
+function main() {
+}
